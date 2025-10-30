bf11f092189895c776d9a1b03bb20b77bc1b71b0
Diogo Grings Graebin <diogograebin@gmail.com>
2025-10-29 21:34:24 -0300

    Adicionar arquivos de projeto.

---
+++ EstoqueMVC/Data/EstoqueContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EstoqueMVC.Models;
+
+namespace EstoqueMVC.Data
+{
+    public class EstoqueContext : DbContext
+    {
+        public EstoqueContext(DbContextOptions<EstoqueContext> options) : base(options)
+        {
+        }
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ EstoqueMVC/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EstoqueMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do produto é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A quantidade do produto é obrigatória.")]
+        public int Quantidade { get; set; }
+
+        [Required(ErrorMessage = "O preço do produto é obrigatório.")]
+        public decimal Preco { get; set; }
+    }
+}
